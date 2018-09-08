9d49fbd64f7d94685537b980680fc7a199746dc7
2GBand <43090368+2GBand@users.noreply.github.com>
2018-09-08 21:50:42 +0630

    Update walletmodel.cpp

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -321,7 +321,7 @@ WalletModel::SendCoinsReturn WalletModel::prepareTransaction(WalletModelTransact
         }
         // reject insane fee
         unsigned int insanefee = (transaction.getTransactionSize() == 0 ? nAddresses : transaction.getTransactionSize());
-        insanefee = ((MIN_RELAY_TX_FEE * insanefee) * 1000000);
+        insanefee = ((MIN_RELAY_TX_FEE * insanefee) * 10000);
         if (nFeeRequired > insanefee){
             LogPrintf("nFeeRequired: %d -- InsaneFee: %d\n", nFeeRequired, insanefee);
             return InsaneFee;
